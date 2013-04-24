601271530eb85a547ec1056b1937e156d0c3083c
trevhud <trevhud@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-24 20:59:11 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/medicare-quote-widget/trunk@703075 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ medicare-quote.php
@@ -0,0 +1,55 @@
+<?php 
+/*
+Plugin Name: Medicare Quote Widget
+Plugin URI: http://blog.ehealthmedicare.com/medicare-quote-widget
+Description: Sends entered zip code to the eHealth Medicare census page for a Medicare quote
+Author: Trevor Hudson - eHealth Medicare	
+Version: 1.0
+Author URI: http://www.ehealthmedicare.com
+*/
+
+define('MedicareQuote_Path', plugin_dir_url( __FILE__ ));
+
+class MedicareQuote extends WP_Widget {
+          function MedicareQuote() {
+                    $widget_ops = array(
+                    'classname' => 'MedicareQuote',
+                    'description' => 'Sends entered zip code to the eHealth Medicare census page for a Medicare quote'
+          );
+
+          $this->WP_Widget(
+                    'MedicareQuote',
+                    'Medicare Quote Widget',
+                    $widget_ops
+          );
+}
+
+          function widget($args, $instance) { // widget sidebar output
+                    extract($args, EXTR_SKIP);
+                    echo $before_widget; // pre-widget code from theme
+print<<<EOM
+
+<div style="border:2px solid; padding:10px; border-radius:25px; box-shadow: 8px 8px 5px #888888;">
+       <h2 style="font-size:24px; color:#092f5c; text-align:center">Get a Free Medicare Quote!</h2>
+   <form action="http://www.ehealthmedicare.com/find-coverage?allid=Med35993" method="post" target="blank">
+ <p style="font-size:14px; text-align:center">Zip Code:<input type="text" name="zip" style="width:60px"></p>
+ <p style="text-align:center"><input type="image" src="/wp-content/plugins/medicare-quote/assets/img/ehealth-medicare-go.png" title="Find Medicare Insurance Options" alt="Find Medicare Insurance Options" class="go-btn"></p>
+</form>
+<p style="text-align:center"><a href= "http://www.ehealthmedicare.com?allid=Med35993" target= "blank"><img src="/wp-content/plugins/medicare-quote/assets/img/ehealth-medicare-logo.gif" align="middle"></a></p>
+
+      </div>
+
+
+EOM;
+
+                    echo $after_widget; // post-widget code from theme
+          
+    }
+}
+
+add_action(
+          'widgets_init',
+          create_function('','return
+      register_widget("MedicareQuote");')
+);
+    ?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Medicare Quote Widget ===
+Contributors: trevor, eHealth Medicare
+Donate link: http://www.ehealthmedicare.com
+Tags: Medicare, eHealth, eHealthMedicare
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.0
+License: GPLv2 or later
+
+Medicare Quote Widget helps visitors get Medicare quotes.
+
+== Description ==
+
+Medicare Quote Widget allows site owners to display a form that sends a zip code to the eHealth Medicare census page so that visitors can get Medicare quotes.
+
+*Note: This plugin calls back to http://www.ehealthmedicare.com for data. It uses a "post" function to send the zip code entered into the widget to the eHealth Medicare census page.
+
+== Installation ==
+
+Upload the Medicare Quote Widget plugin to your blog, and activate it.
+
+Go to Appearance/widgets to add the Medicare Quote Widget to your blog.
+
+== Frequently Asked Questions ==
+
+How do I find out more information about Medicare?
+
+You can go to www.medicare.gov to learn more about the Medicare program.
+
+== Changelog ==
+
+So far, no changes have been made to the Medicare Quote Widget plugin.
+
+== Upgrade Notice ==
+
+Please email trevor.hudson@ehealth.com for upgrade suggestions.
+
+== Screenshots ==
+
+This is how the widget looks in a sidebar.
